f59f5556795341072699728e0d145c67b539e2bc
Ellen <elcristine.ps@gmail.com>
2021-10-24 19:34:57 -0400

    Primeira versão

---
+++ src/orcamento/code/Formatter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orcamento.code;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ *
+ * @author eharv
+ */
+public class Formatter {
+
+    public String format(double valor) {
+
+        String valorFormatado = NumberFormat.getCurrencyInstance(Locale.getDefault()).format(valor);
+        return valorFormatado;
+
+    }
+
+}
---
+++ src/orcamento/code/SystemClipboard.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orcamento.code;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+
+/**
+ *
+ * @author eharv
+ */
+public class SystemClipboard {
+   
+    public static void copiar (String text){
+         
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(new StringSelection(text), null);
+        
+    }
+   
+}
